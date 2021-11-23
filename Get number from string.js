f9ae4b6c76adc32ea39284561c7411fa4bcc1ee1
sw30041995 <sw1225@yandex.ru>
2021-11-23 20:59:26 +0300

    new file

---
+++ Get number from string.js
@@ -0,0 +1,10 @@
+function getNumberFromString(s) {
+  let num = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '0' || s[i] === '1' || s[i] === '2' || s[i] === '3' ||s[i] === '4' || s[i] === '5' ||
+        s[i] === '6' || s[i] === '7' || s[i] === '8' || s[i] === '9') {
+      num += s[i];
+    }
+  }
+  return Number(num);
+}
